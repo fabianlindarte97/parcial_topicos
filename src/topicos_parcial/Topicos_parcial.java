ef4fa708980a44413bc5598015b3c7053b7b9891
Fabian Lindarte <Fabian Lindarte@DESKTOP-DTG4H7V>
2020-06-15 11:45:22 -0500

    Version Final

--- src/topicos_parcial/Topicos_parcial.java
+++ src/topicos_parcial/Topicos_parcial.java
@@ -8,6 +8,9 @@ public class Topicos_parcial {
      */
     public static void main(String[] args) {
         System.out.println("Version Master");
+        System.out.println("Version Desarrollador1");
+        System.out.println("Version Desarrollador2");
+        System.out.println("Version Desarrollador3");
     }
     
 }
